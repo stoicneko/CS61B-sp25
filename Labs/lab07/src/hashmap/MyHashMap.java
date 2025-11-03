83c96e85858db6b743b2a4930ee3ed270669784e
stoicneko <anchor1618@outlook.com>
2025-11-03 22:31:24 +0800

    开始 lab07

--- Labs/lab07/src/hashmap/MyHashMap.java
+++ Labs/lab07/src/hashmap/MyHashMap.java
@@ -1,13 +1,17 @@
 package hashmap;
 
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
+ * @author stoicneko
+ * @version 1.0
+ * @since 2025.10.26
  *  A hash table-backed Map implementation.
- *
  *  Assumes null keys will never be inserted, and does not resize down upon remove().
- *  @author stoicneko
- */
+ **/
+
 public class MyHashMap<K, V> implements Map61B<K, V> {
 
     /**
@@ -26,13 +30,25 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
 
     /* Instance Variables */
     private Collection<Node>[] buckets;
-    // You should probably define some more!
+    private int size = 0;
+    private double loadFactor;
+    private int initialCapacity;
+
+    // 为什么需要三个构造函数?
+    // 三个状态, 三种不同输入
 
     /** Constructors */
-    public MyHashMap() { }
+    // 最固定的, 默认的
+    public MyHashMap() {
+        this(16, 0.75);
+    }
 
-    public MyHashMap(int initialCapacity) { }
+    public MyHashMap(int initialCapacity) {
+        this(initialCapacity,0.75);
 
+    }
+
+    // 最自由的
     /**
      * MyHashMap constructor that creates a backing array of initialCapacity.
      * The load factor (# items / # buckets) should always be <= loadFactor
@@ -40,7 +56,13 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * @param initialCapacity initial size of backing array
      * @param loadFactor maximum load factor
      */
-    public MyHashMap(int initialCapacity, double loadFactor) { }
+    public MyHashMap(int initialCapacity, double loadFactor) {
+        if (initialCapacity <= 0 || loadFactor <= 0) {
+            throw new IllegalArgumentException();
+        }
+        buckets = new Collection[initialCapacity];
+
+    }
 
     /**
      * Returns a data structure to be a hash table bucket
@@ -69,5 +91,46 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
 
     // TODO: Implement the methods of the Map61B Interface below
     // Your code won't compile until you do so!
+
+    @Override
+    public void put(K key, V value) {
+
+        size += 1;
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    @Override
+    public boolean containsKey(K key) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
 
 }
